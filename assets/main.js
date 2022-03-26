3826d7ef221da287859915ad400ac4c0fc902039
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-26 18:35:41 +1100

    added Javascript for array of days, function to loop through them

---
+++ assets/main.js
@@ -0,0 +1,35 @@
+function getInfo() {
+	// Collects data from input field //
+	const newName = document.getElementById("cityInput");
+	const cityName = document.getElementById("cityName");
+	// Displays data enterred to input //
+	cityName.innerHTML = "--" + newName.value + "--";
+	//Passing the API key & city name//
+	fetch("http://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=3649b0b86df7a1e0a5f6def57b72b739")
+		//Javascript promise that will return the data//
+		.then((Response) => Response.json())
+		.then((data) => {
+			for (i = 0; i < 5; i++) {
+				document.getElementById("day" + (i + 1) + "Min").innerHTML = "Min" + Number(data.list[i].main.temp_min - 288.53).toFixed(1) + "°";
+			}
+			for (i = 0; i < 5; i++) {
+				document.getElementById("day" + (i + 1) + "Max").innerHTML = "Max" + Number(data.list[i].main.temp_max - 288.53).toFixed(1) + "°";
+			}
+			//Loops through list to find selected icon//
+			for (i = 0; i < 5; i++) {
+				document.getElementById("img" + (i + 1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+			}
+		});
+	// .catch(err => alert("something went wrong"));
+}
+const daySelection = new Date();
+//Array of all days of the week//
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//Gets correct integer for index of days in array//
+function dayCheck(day) {
+	if (day + daySelection.getDay() > 6) {
+		return day + daySelection.getDay() - 7;
+	} else {
+		return day + daySelection.getDay();
+	}
+}
