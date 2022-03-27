beb66094a5336c735cb70039084702b9d2d2ede1
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-27 23:05:45 +1100

    removed time

--- assets/main.js
+++ assets/main.js
@@ -16,7 +16,6 @@ function getInfo() {
 		//Javascript promise that will return the data//
 		.then((Response) => Response.json())
 		.then((data) => {
-			console.log(data);
 			for (i = 0; i < 5; i++) {
 				document.getElementById("day" + (i + 1) + "Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min - 273.15).toFixed(1) + "°";
 			}
@@ -32,20 +31,17 @@ function getInfo() {
 			document.getElementById("currentDay").innerHTML = " " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
 			document.getElementById("currentWind").innerHTML = " " + Number(data.list[i].wind.speed - 3.25).toFixed(1);
 			document.getElementById("currentHumidity").innerHTML = Number(data.list[i].main.humidity - 62).toFixed(1);
-			let date = new Date(data.list[i].dt_txt);
-			document.getElementsByClassName("Date" + (i + 1) + "Date").innerHTML = "Date: " + (1 + date.getMonth().toString().padStart(2, "0")) + "/" + date.getDate().toString().padStart(2, "0") + "/" + date.getFullYear();
 			//Loops through list to find selected icon//
 			for (i = 0; i < 5; i++) {
 				document.getElementById("img" + (i + 1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
 			}
+
 			fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.city.coord.lat + "&lon=" + data.city.coord.lon + "&appid=3649b0b86df7a1e0a5f6def57b72b739")
 				.then((Response) => Response.json())
 				.then((data) => {
-					console.log(data);
 					document.getElementById("currentUV").innerHTML = " " + Number(data.current.uvi - 0).toFixed(1);
 				});
 		});
-
 	try {
 	} catch (error) {
 		alert("something went wrong");
