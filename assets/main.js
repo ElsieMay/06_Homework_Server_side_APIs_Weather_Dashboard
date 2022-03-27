55ba0af9d5d43f2d1ca54c625644187f20383a43
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-27 21:43:54 +1100

    added uvi

--- assets/main.js
+++ assets/main.js
@@ -28,21 +28,20 @@ function getInfo() {
 			for (i = 0; i < 5; i++) {
 				document.getElementById("day" + (i + 1) + "Wind").innerHTML = "Wind:" + Number(data.list[i].wind.speed - 3.25).toFixed(1);
 			}
-			for (i = 0; i < 5; i++) {
-				document.getElementById("current" + (i + 1) + "Day").innerHTML = " " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
-			}
-			// for (i = 0; i < 5; i++) {
-			// 	document.getElementsByClassName("timeZone" + (i + 1) + "timeZone").innerHTML = "" + Number(data.list[i].city.timezone - 0).toFixed(1);
-			// // }
-			for (i = 0; i < 5; i++) {
-				let date = new Date(data.list[i].dt_txt);
-				console.log(date);
-				document.getElementsByClassName("Date" + (i + 1) + "Date").innerHTML = "Date: " + (1 + date.getMonth().toString().padStart(2, "0")) + "/" + date.getDate().toString().padStart(2, "0") + "/" + date.getFullYear();
-			}
+			document.getElementById("currentDay").innerHTML = " " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+			document.getElementById("currentWind").innerHTML = " " + Number(data.list[i].wind.speed - 3.25).toFixed(1);
+			document.getElementById("currentHumidity").innerHTML = Number(data.list[i].main.humidity - 62).toFixed(1);
+			let date = new Date(data.list[i].dt_txt);
+			document.getElementsByClassName("Date" + (i + 1) + "Date").innerHTML = "Date: " + (1 + date.getMonth().toString().padStart(2, "0")) + "/" + date.getDate().toString().padStart(2, "0") + "/" + date.getFullYear();
 			//Loops through list to find selected icon//
 			for (i = 0; i < 5; i++) {
 				document.getElementById("img" + (i + 1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
 			}
+			fetch("https://api.openweathermap.org/data/2.5/onecall?q" + newName.value + "&appid=3649b0b86df7a1e0a5f6def57b72b739")
+				.then((Response) => Response.json())
+				.then((data) => {
+					document.getElementById("currentUV").innerHTML = "" + Number(data.list[i].current.uvi - 0.89).toFixed(1);
+				});
 		});
 
 	try {
